726e7f20034eb6e47e2c4434fea1caf6916c9aa6
austinc512 <85425656+austinc512@users.noreply.github.com>
2023-07-05 20:25:32 -0500

    Pig Latin works in GUI

--- main.js
+++ main.js
@@ -1,74 +1,30 @@
 "use strict";
 
-// brings in the assert module for unit testing
-const assert = require("assert");
-// brings in the readline module to access the command line
-const readline = require("readline");
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// got rid of tests for this branch, and attached input and output DOM elements to make this logic usable in a GUI.
 
-const pigLatin = (word) => {
-  let input = word.trim().toLowerCase();
-  const vowels = ["a", "e", "i", "o", "u"];
+const vowels = ["a", "e", "i", "o", "u"];
+
+document.getElementById("submit").addEventListener("click", function (e) {
+  e.preventDefault();
+  const input = document
+    .getElementById("user-input")
+    .value.trim()
+    .toLowerCase();
+
+  let output = "";
   if (vowels.includes(input[0])) {
-    return `${input}yay`;
+    output = `${input}yay`;
   } else if (
     !vowels.includes(input[0]) &&
     !vowels.includes(input[1]) &&
     !vowels.includes(input[2])
   ) {
-    return `${input.slice(3)}${input.slice(0, 3)}ay`;
+    output = `${input.slice(3)}${input.slice(0, 3)}ay`;
   } else if (!vowels.includes(input[0]) && !vowels.includes(input[1])) {
-    return `${input.slice(2)}${input.slice(0, 2)}ay`;
+    output = `${input.slice(2)}${input.slice(0, 2)}ay`;
   } else {
-    return `${input.slice(1)}${input[0]}ay`;
+    output = `${input.slice(1)}${input[0]}ay`;
   }
-};
-
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
-const getPrompt = () => {
-  rl.question("word ", (answer) => {
-    console.log(pigLatin(answer));
-    getPrompt();
-  });
-};
-
-// Unit Tests
-// to use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === "function") {
-  describe("#pigLatin()", () => {
-    it("should translate a simple word", () => {
-      assert.equal(pigLatin("car"), "arcay");
-      assert.equal(pigLatin("dog"), "ogday");
-    });
-    it("should translate a complex word", () => {
-      assert.equal(pigLatin("create"), "eatecray");
-      assert.equal(pigLatin("valley"), "alleyvay");
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin("egg"), "eggyay");
-      assert.equal(pigLatin("emission"), "emissionyay");
-    });
-    it("should lowercase and trim word before translation", () => {
-      assert.equal(pigLatin("HeLlO "), "ellohay");
-      assert.equal(pigLatin(" RoCkEt"), "ocketray");
-    });
-  });
-} else {
-  getPrompt();
-}
-
-// **********
-//   HINTS
-// **********
-
-// break your code into pieces and focus on one piece at a time...
-// 1. if word begins with a vowel send to one function: adds "yay"
-// 2. if word begins with a consonant send to another function: splices off beginning, returns word with new ending.
-// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
+  // console.log(output);
+  document.getElementById("log1").innerHTML = output;
+});
